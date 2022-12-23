f9979af0e44ca7a79e57646f2a714a7feb615464
Jie Wang <121305629+MedDataInt@users.noreply.github.com>
2022-12-23 18:14:53 -0500

    Add files via upload
    Venn diagram with Python(with values) and R(with gene list)

---
+++ Venn diagram.py
@@ -0,0 +1,16 @@
+### venn diagram 
+
+# basic
+from bioinfokit import visuz
+visuz.venn(vennset=(1595,133,196,522,789,96,133), vennlabel = ('Human', 'Mouse', 'CellLine'))
+# note: order the set in (100,010,110,001,101,011,111) format
+
+
+# change default colors
+visuz.venn(vennset=(1595,133,196,522,789,96,133), vennlabel = ('Human', 'Mouse', 'CellLine'),
+venncolor=('#ff6347', '#008000', '#FF00FF'))
+
+# change default transparency 
+visuz.venn(vennset=(1595,133,196,522,789,96,133), vennlabel = ('Human', 'Mouse', 'CellLine'),
+venncolor=('#ff6347', '#008000', '#FF00FF'), vennalpha=0.9)
+
